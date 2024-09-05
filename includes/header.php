45acd382609a119bd329c323bdea3e13abe6c863
eliyasakondo <eliyasakondo@gmail.com>
2024-09-05 18:51:39 +0600

    header and style updated

--- includes/header.php
+++ includes/header.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title> <?php echo $pageTitle; ?></title>
-    <link rel="stylesheet" href="/assets/css/styles.css">
+    <link rel="stylesheet" href="../assets/css/style.css">
     <?php if(isset($tutor_register)): ?>
     <link rel="stylesheet" href="<?php echo $tutor_register; ?>">
     <?php endif; ?>
